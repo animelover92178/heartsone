62c9b1a730c811eb6d280eb04a91b7926bfe800a
bayu <bayu@katadata.co.id>
2018-11-03 14:33:22 +0700

    fbase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {LoaderService}from './shared/service/loader.service';
 import {AlertService}from './shared/service/alert.service';
+import {AngularFireModule}from '@angular/fire';
+import {AngularFirestore, AngularFirestoreModule}from '@angular/fire/firestore';
+import {Firebase} from '@ionic-native/firebase';
+const config ={
+    apiKey: "AIzaSyCBitO8_DU75n93tp1NMbpIq-pGLZuPT6Y",
+    authDomain: "ng-recipe-book-69bb2.firebaseapp.com",
+    databaseURL: "https://ng-recipe-book-69bb2.firebaseio.com",
+    projectId: "ng-recipe-book-69bb2",
+    storageBucket: "ng-recipe-book-69bb2.appspot.com",
+    messagingSenderId: "794132821485"
+  };
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,IonicStorageModule.forRoot()],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    IonicStorageModule.forRoot(),
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
+  
+  
+  ],
   providers: [
     StatusBar,
     LoaderService,
     ToastService,
     SplashScreen,
     AlertService,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
---
+++ src/app/shared/service/fcm.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core'
+import {Firebase}from '@ionic-native/firebase/ngx'
+import {Platform} from '@ionic/angular'
+import {AngularFirestore}from '@angular/fire/firestore'
+@Injectable()
+export class FcmService{
+    constructor(private firebase:Firebase,private platform:Platform,private afs:AngularFirestore){
+
+    }
+    async getToken(){
+        let token;
+        if(this.platform.is('android')){
+            token = await this.firebase.getToken();
+        }
+        if(this.platform.is('ios')){
+            token = await this.firebase.getToken();
+            await this.firebase.grantPermission();
+        }
+        this.saveToken(token);
+    }
+    private saveToken(token:string){
+        if(!token) return;
+        const deviceRef = this.afs.collection('devices');
+        const data = {
+            token,
+            userId:'testUserId'
+        }
+        return deviceRef.doc(token).set(data);
+    }
+    oNotifications(){
+        return this.firebase.onNotificationOpen();
+    }
+}
